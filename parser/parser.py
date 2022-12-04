38a322ba1ebfac4ea08000ca766c3260aaa27c70
Mihai Cherechesu <mihai.cherechesu@gmail.com>
2022-12-04 16:15:04 +0200

    Add health checks for parser and pull image always
    Signed-off-by: Mihai Cherechesu <mihai.cherechesu@gmail.com>

--- parser/parser.py
+++ parser/parser.py
@@ -1,4 +1,6 @@
 from schema import Schema, And, SchemaError, Optional
+from flask import Flask, request, Response, json
+from werkzeug.exceptions import BadRequest, NotFound
 
 
 class Parser:
@@ -13,9 +15,28 @@ class Parser:
         })
 
 
+app = Flask(__name__)
+parser = Parser()
+
+
 def main():
-    print("initializing parser...")
-    parser = Parser()
+    print("starting to listen on :8082")
+    app.run(host="0.0.0.0", port=8082)
+
+
+@app.route("/parse", methods=["POST"])
+def parse_pipeline():
+    try:
+        parser.schema.validate(request.get_json())
+    except SchemaError as e:
+        return Response("parsing failed, reason: " + str(e), status=500)
+
+    return Response("parsing successful", status=200)
+
+
+@app.route("/health", methods=["GET"])
+def check_health():
+    return Response("parser is healthy", status=200)
 
 
 if __name__ == "__main__":
