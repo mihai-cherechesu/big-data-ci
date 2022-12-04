386990f368ff4f6b4a6537c194ba9273415f41fa
Mihai Cherechesu <mihai.cherechesu@gmail.com>
2022-12-04 11:33:15 +0200

    Add schema definition and tests for parser
    Signed-off-by: Mihai Cherechesu <mihai.cherechesu@gmail.com>

--- parser/parser.py
+++ parser/parser.py
@@ -1,7 +1,22 @@
-from schema import Schema
+from schema import Schema, And, SchemaError, Optional
+
+
+class Parser:
+    def __init__(self) -> None:
+        self.schema = Schema({
+            "image": str,
+            And(str, lambda s: s not in ("image")): {
+                "script": [str],
+                Optional("depends_on"): [str],
+                Optional("artifacts"): [str]
+            }
+        })
+
 
 def main():
-    print(Schema(int).validate(123))
+    print("initializing parser")
+    parser = Parser()
+
 
 if __name__ == "__main__":
     main()
